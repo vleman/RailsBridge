eeb68abd9c42314ee35f3339ca893c10a07e3128
Victor Leman <victor.leman@gmail.com>
2016-09-24 15:36:06 -0400

    description removed, links created

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -52,7 +52,7 @@ class TopicsController < ApplicationController
   end
 
   # DELETE /topics/1
-  # DELETE /topics/1.jso
+  # DELETE /topics/1.json
   def destroy
     @topic.destroy
     respond_to do |format|
